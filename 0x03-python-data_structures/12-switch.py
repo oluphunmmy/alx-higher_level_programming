aab15a89190cfde9b9ac3c45917204ec5e23b494
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-03-14 02:16:56 -0700

    fixed

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        del(my_list[idx])
-    return(my_list)
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
