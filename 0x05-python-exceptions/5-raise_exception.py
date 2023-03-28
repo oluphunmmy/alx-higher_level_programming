a68cb1b456b40a94b47dbc0657a6f583414c12d3
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-03-27 21:18:42 -0700

    updated

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
     raise TypeError
-
-
-
-
-vi 6-raise_exception_msg.py
-
-#!/usr/bin/python3
-def raise_exception_msg(message=""):
-    raise NameError(message)
