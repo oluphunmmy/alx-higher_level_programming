afe80169e266ac91eee612541407ae7f7d9517c8
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-06-19 21:53:01 -0700

    ORM SQL

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 """
-creates the State California with the City San Francisco from
-the database hbtn_0e_100_usa. The script should take 3 arguments:
-mysql username, mysql password and database name
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
 """
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Create a connection
+    Access to the database and get the cities
+    from the database.
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Base.metadata.create_all(engine)
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
 
-    new_state = State(name='California')
-    new_city = City(name='San Francisco')
-    new_state.cities.append(new_city)
+    session = Session()
+    cal_state = State(name='California')
+    sfr_city = City(name='San Francisco')
+    cal_state.cities.append(sfr_city)
 
-    session.add(new_state)
-    session.add(new_city)
-
+    session.add(cal_state)
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
 """
-State model with SqlAchemy
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
+
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ class definition of a State """
+    """State class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+        cities (:obj:`City`): The Cities belongs to State
+
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete")
+    cities = relationship("City", backref="state", cascade="all, delete")
