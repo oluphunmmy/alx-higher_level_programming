c67e2071066b4ff9196517773044d78146b2b05c
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-07-02 17:01:00 -0700

    recheck

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
+'''Module:
+find a peak in a list of unsorted integers.
+'''
 
 
 def find_peak(list_of_integers):
-    """ Finds the peak in a list of integers """
-    if list_of_integers == []:
+    '''
+    find a peak in a list of unsorted integers.
+
+    args:
+        list_of_integers (list): list of integers.
+    returns:
+        int: peak element.
+    '''
+    if not list_of_integers:
         return None
 
-    length = len(list_of_integers)
-    m = int(length / 2)
-    li = list_of_integers
+    start = 0
+    end = len(list_of_integers) - 1
 
-    if m - 1 < 0 and m + 1 >= length:
-        return li[m]
-    elif m - 1 < 0:
-        return li[m] if li[m] > li[m + 1] else li[m + 1]
-    elif m + 1 >= length:
-        return li[m] if li[m] > li[m - 1] else li[m - 1]
+    while start < end:
+        mid = (start + end) // 2
 
-    if li[m - 1] < li[m] > li[m + 1]:
-        return li[m]
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            start = mid + 1
+        else:
+            end = mid
 
-    if li[m + 1] > li[m - 1]:
-        return find_peak(li[m:])
-    return find_peak(li[:m])
+    return list_of_integers[start]
